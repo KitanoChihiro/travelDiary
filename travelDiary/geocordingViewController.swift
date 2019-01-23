ed90e8ff50d29f088191bcd77189624c5b141533
北野千裕 <chi.t.angel.s2@gmail.com>
2019-01-23 13:47:42 +0800

    ジオコーディングに関するファイルの作成

---
+++ travelDiary/geocordingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  geocordingViewController.swift
+//  travelDiary
+//
+//  Created by 北野千裕 on 2019/01/23.
+//  Copyright © 2019年 Chihiro Kitano. All rights reserved.
+//
+
+import UIKit
+
+class geocordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
